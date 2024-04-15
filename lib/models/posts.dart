7b00dbd97fba5d015d6a1e8e4d44015f3e2e5631
Soniabensaad <5687@holbertonstudents.com>
2024-04-15 23:16:32 +0200

    task5

---
+++ lib/models/posts.dart
@@ -0,0 +1,26 @@
+class Users {
+  String uid;
+  String email;
+  String username;
+  String bio;
+  String photoUrl;
+  List followers;
+  List following;
+  List posts;
+  List saved;
+  String searchKey;
+}
+
+Constructor(
+  this.uid,
+  this.email,
+  thid.username,
+  this.bio,
+  this.photoUrl,
+  this.followers,
+  this. following,
+  this.posts,
+  this.saved,
+  this.searchKey,
+
+);
